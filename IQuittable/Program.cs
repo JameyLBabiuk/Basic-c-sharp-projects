40544e9330365a409254d601343e433943836e64
jamnjeep66 <jamnjeep66@gmail.com>
2023-06-11 15:41:37 +0300

    updated IQuittable/

---
+++ IQuittable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Definition of the IQuittable interface
+public interface IQuittable
+{
+    void Quit();
+}
+
+// Employee class inheriting from the IQuittable interface
+public class Employee : IQuittable
+{
+    public string Name { get; set; }
+
+    public void Quit()
+    {
+        Console.WriteLine("The New Employee has decided to quit their job.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Creating an object of type IQuittable using polymorphism
+        IQuittable quittableEmployee = new Employee();
+
+        // Call the Quit() method on the IQuittable object
+        quittableEmployee.Quit();
+
+        Console.ReadLine();
+    }
+}
